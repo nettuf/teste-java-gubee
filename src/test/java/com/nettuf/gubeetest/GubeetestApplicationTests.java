30bb6b8b34c75bdc77ed236480b3ec92fc66903f
Netto Fernandes <hugkhj@gmail.com>
2021-10-01 11:03:57 -0300

    start

---
+++ src/test/java/com/nettuf/gubeetest/GubeetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettuf.gubeetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubeetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
